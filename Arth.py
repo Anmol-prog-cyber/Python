c465c2b3a67c6b6ac3d8e219db9ec36a2a3400b3
Anmol-prog-cyber <anmolbannu19@gmail.com>
2025-08-09 09:32:12 -0600

    Run

--- Arth.py
+++ Arth.py
@@ -1,1 +1,13 @@
-
+# hash" by providing hash interpreter will skip the line of execution 
+# hash is used for commenting the code adding description for the code snippet
+# hash is used for the explanation of the code 
+a=5 # a is a variable 
+b=5 # b is a variable 
+s=a+b 
+print("Sum =  ",s) # print is a function to print variables in the output screen 
+m=a*b
+print("Multiply =   ",m)
+d=a/b
+print("Divide =   ",d) # / is used to to give a quotient 
+r=a%b
+print("Reminder=  ",r) # % is used to give a reminder 
