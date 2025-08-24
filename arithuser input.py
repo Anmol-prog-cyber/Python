753bb1a9308d863018ae023bd12d20140e4c3ef6
Anmol-prog-cyber <anmolbannu19@gmail.com>
2025-08-24 09:50:18 -0600

    Create arithuser input.py

---
+++ arithuser input.py
@@ -0,0 +1,14 @@
+
+# hash" by providing hash interpreter will skip the line of execution 
+# hash is used for commenting the code adding description for the code snippet
+# hash is used for the explanation of the code 
+a=input("input the value")# string : Default 
+b=input("input the value")
+s=int(a) + int(b)
+print("Sum =  ",s) # print is a function to print variables in the output screen 
+m=int(a) * int(b)
+print("Multiply =   ",m)
+d=int(a) / int(b)
+print("Divide =   ",d) # / is used to to give a quotient 
+r=int(a) % int(b)
+print("Reminder=  ",r) # % is used to give a reminder 
