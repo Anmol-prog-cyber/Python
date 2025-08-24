df5e6a78a207d06c9e21c90f720e9179b4f0a3d7
Anmol-prog-cyber <anmolbannu19@gmail.com>
2025-08-24 09:36:42 -0600

    Create userinput2.py

---
+++ userinput2.py
@@ -0,0 +1,26 @@
+'''
+data type
+
+int ---> integer (Number )(-2,-1,0,1,2,.......)
+char ---> character ('a') ('b') [ one among a to z]
+str ---> string ( collection of character) ("anmol")
+
+8 ---> integer
+'k'---> character
+"Good" ---> string
+
+-4 -->integer
+"welcome" ----> string
+'c' ---> char
+
+'''
+
+a = input("input the value") # string : default
+# input the value 9(int)---input()--->"9" (string)
+
+b = input("input the value")
+
+c = int(a) +int(b) # string + integer 
+#type casting ---> change data types to different type
+
+print("sum ",c)
