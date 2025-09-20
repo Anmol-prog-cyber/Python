b793677755f5d86e73a893384e39b104e26be738
Anmol-prog-cyber <anmolbannu19@gmail.com>
2025-09-20 09:56:46 -0600

    Create Controlstatement.py

---
+++ Controlstatement.py
@@ -0,0 +1,42 @@
+'''
+Conditional or control statement
+
+father : if you score 90% then i will gift you mac book
+
+scroing 90 % ----> condition
+
+son : if son meet condition then he will get mac book
+      if son does not meet condition then he will get nothing
+
+
+In computer:
+
+syntax for control statement
+
+
+if condition:
+
+  # write your action statement if condition meet
+else:
+
+  # write your action statement if condition not meet
+
+
+'''
+#----------------------------------------------
+# usecases
+'''
+age for vote
+ticket fare
+age group
+pass pencentage
+
+'''
+#-------------------------------------------------
+
+age = int(input("Enter your age"))
+
+if age >= 18: # age >= 18 ---> condition ---> true --> then below statement will be executed
+    print("you are eligible to vote")
+else:
+    print("!! you are not eligible to vote")
