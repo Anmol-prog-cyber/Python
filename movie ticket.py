bda5801e7303ba3ae0bd3a0eb91013a8f932cf0f
Anmol-prog-cyber <anmolbannu19@gmail.com>
2025-09-20 10:14:49 -0600

    Create movie ticket.py

---
+++ movie ticket.py
@@ -0,0 +1,5 @@
+age = int(input("Enter your age")) # This is an input statement 
+if age <= 4: # Control Statement 
+    print("movie ticket is free")# Print statement 
+else:# Control statement or choice statement for true or false whether age x  > 4  or age x < 4
+    print("you have to pay for the movie ticket") # Print statement for running the prompt 
