eb3460d83423075567b7526db21a9175e2d246e6
Anmol-prog-cyber <anmolbannu19@gmail.com>
2025-09-20 10:30:30 -0600

    Create Code share.py

---
+++ Code share.py
@@ -0,0 +1,8 @@
+https://codeshare.io/ame8lw
+https://www.onlinegdb.com/online_python_compiler#
+bharanidharan s
+10:27 AM
+get input --> mark
+
+
+if your mark is greater then 40 then you are pass else you are fail
