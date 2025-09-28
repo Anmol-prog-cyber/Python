7c8b01efc017302b396f5226d52900ac15733786
Anmol-prog-cyber <anmolbannu19@gmail.com>
2025-09-28 10:16:49 -0600

    Create userinput.py

---
+++ userinput.py
@@ -0,0 +1,4 @@
+Algebraicexpression= (input("Enter the algebraic Expression"))
+name= (input("Enter your name"))
+DateofBirth =(input("Enter your date of birth")) 
+Dateoftoday = (input("Enter the date of today "))
