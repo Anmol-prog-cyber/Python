1762eaf15f3318913e46c000bb2642701a23cf3f
Anmol-prog-cyber <anmolbannu19@gmail.com>
2025-10-04 09:56:22 -0600

    Create whileLoop.py

---
+++ whileLoop.py
@@ -0,0 +1,22 @@
+# Looping statement
+# to execute same statement for n number of times we use loop.
+# in Loop you can mention the condition how many time loop to be executed
+
+# 3 types of Loops
+# while Loop, do while loop , for loop
+# -----------------------------------        
+# syntax for while loop
+    # while condition:
+            # statements
+    
+    # if condition is true then loop will be executed
+    # if condition is false then loop will not be executed
+    
+#----------------------------  
+
+n = 1 # Initialization
+
+while n<3: # condition n<10
+  print("Inside loop anmol")
+  n = n + 1
+print("outside loop")
