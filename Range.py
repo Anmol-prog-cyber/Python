35e9f14706947e35938d2183a80289d21feff5db
Anmol-prog-cyber <anmolbannu19@gmail.com>
2025-10-05 10:05:56 -0600

    Create Range.py

---
+++ Range.py
@@ -0,0 +1,16 @@
+# Looping statement
+# to execute same statement for n number of times we use loop.
+# in Loop you can mention the condition how many time loop to be executed
+
+# 3 types of Loops
+# while Loop(entry check loop), do while loop (exit check loop), for loop
+# -----------------------------------
+# syntax for for loop
+     
+     #for variable in range(value):
+         #loop statement
+    
+#----------------------------  
+
+for x in range(10,-1,-5): # range(1)---> 10,9,8,7,6,5,4,3,2,1,0
+   print(x)
