ef3f8a4ea597add4181009cbbb6c5494b0632580
Anmol-prog-cyber <anmolbannu19@gmail.com>
2025-10-05 10:16:17 -0600

    Create numericals.py

---
+++ numericals.py
@@ -0,0 +1,21 @@
+
+# Looping statement
+# to execute same statement for n number of times we use loop.
+# in Loop you can mention the condition how many time loop to be executed
+
+# 3 types of Loops
+# while Loop(entry check loop), do while loop (exit check loop), for loop
+# -----------------------------------
+# syntax for for loop
+     
+     #for variable in range(value):
+         #loop statement
+    
+#----------------------------  
+
+for x in range(0,10000000000,+100000): # range(1)---> 0,5,10,15,20..... 
+   print(x)  # 0 represents the starting point 
+              # 1000 represents the ending point of the numbers 
+              # every number is increasing value of +100 
+              # Works for negative numbers too when the sign changes 
+              
