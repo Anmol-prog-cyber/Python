42b9d83601327c3ace54efdc689ad14988c40f5a
Anmol-prog-cyber <anmolbannu19@gmail.com>
2025-10-12 09:31:52 -0600

    Create test1,question2.py

---
+++ test1,question2.py
@@ -0,0 +1,12 @@
+ three variables and get three values from user input and add all three values and print the sum 
+
+E =90 # x is a variable 
+F =91 # y is a variable 
+G =92 # z is a variable  
+
+print(E) #90
+print(F) #91
+print(G) #92
+
+Addition= E+F+G
+print(Addition)
