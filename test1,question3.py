fa03ff3f4fd3f7b62ccabe9fdad4828de289b284
Anmol-prog-cyber <anmolbannu19@gmail.com>
2025-10-12 09:50:05 -0600

    Create test1,question3.py

---
+++ test1,question3.py
@@ -0,0 +1,15 @@
+ # Question 2:
+
+
+# Declare three variables and get three values from user input and add all three values and print the sum 
+
+E=int(input("Enter number 1"))
+F=int(input("Enter number 2"))
+G=int(input("Enter number 3"))
+
+print(E) 
+print(F) 
+print(G) 
+
+Addition= E+F+G
+print(Addition)
