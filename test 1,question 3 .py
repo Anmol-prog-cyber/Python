d9afdcd61ca8c5242a770e7d858a5de168d5d6f5
Anmol-prog-cyber <anmolbannu19@gmail.com>
2025-10-12 10:11:53 -0600

    Create test 1,question 3 .py

---
+++ test 1,question 3 .py
@@ -0,0 +1,21 @@
+
+# Question 3:
+
+ # Declare two variable and get the two user input and print the largest of two number
+ 
+
+F=int(input("Enter number 1"))
+H=int(input("Enter number 2"))
+
+ # print(F)
+ # print(H) 
+
+ # if age >= 18: # age >= 18 ---> condition ---> true --> then below statement will be executed
+    # print("you are eligible to vote")
+ # else:
+    # print("!! you are not eligible to vote") 
+
+if F > H : 
+    print(F)
+else: 
+    print(H) 
