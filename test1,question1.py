735b135c3aaa999164947a718f8d813188d5c1be
Anmol-prog-cyber <anmolbannu19@gmail.com>
2025-10-12 09:14:27 -0600

    Create test1,question1.py

---
+++ test1,question1.py
@@ -0,0 +1,10 @@
+
+# Question 1:
+# Declare two variables and initialize two variable with any two number and print the value of the two variables
+ 
+a=67 # a is a variable 
+b=57 # b is a variable 
+c=47 # c is a variable 
+print(a)
+print(b)
+print (c)
