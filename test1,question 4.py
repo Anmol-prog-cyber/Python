60e12a2a6a06119e3f27cdad8b9049dd432c534c
Anmol-prog-cyber <anmolbannu19@gmail.com>
2025-10-18 09:04:38 -0600

    Create test1,question 4.py

---
+++ test1,question 4.py
@@ -0,0 +1,20 @@
+ #Question 4:
+ # Write a program to take two inputs from user and use all arithmetic operators and print the results
+
+a=input("enter the first number")
+b=input("enter the second number")
+
+s=int(a) + int(b)
+print("Sum +",s) 
+
+subtract=int(a)-int(b)
+print("Subtract",subtract) 
+
+multiply=int(a)*int(b)
+print("multiply" ,multiply) 
+
+divide=int(a)/int(b)
+print("divide" ,divide) 
+
+percentage=int(a)%int(b) # % is used to give a reminder
+print("percentage"  ,percentage) # r=int(a) % int(b)
