6ce76a6f9ce747501b7054603f3c9666cb02e4c3
Anmol-prog-cyber <anmolbannu19@gmail.com>
2025-10-18 09:18:59 -0600

    Create Test1,question5.py

---
+++ Test1,question5.py
@@ -0,0 +1,23 @@
+ #Question 5:
+
+ #Write a program to print numbers from 1 to 10
+
+#Sample Input and output
+
+#1
+#2
+#3
+#4
+#5
+#6
+#7
+#8
+#9
+#10
+
+
+ #for index in range(1100): # range(1)---> 0,1,2,3,4,5,6,7,8,9,101,11,12
+     # print("Anmol",index). 
+     
+for index in range(1,11): #range(1)--0,1,2,3,4,5,6,7,8,9
+ print(index)
