ba51d0e6dbf8f22477f437c27828da760f7776d9
Anmol-prog-cyber <anmolbannu19@gmail.com>
2025-10-19 10:40:53 -0600

    Create Sumofendnumber.py

---
+++ Sumofendnumber.py
@@ -0,0 +1,8 @@
+Sum =0 
+startnumber = 1 
+Endnumber = int(input("Enter number "))
+
+while startnumber <= Endnumber:
+    Sum = Sum + startnumber
+    startnumber = startnumber+1 
+print("Sum = Anmol", Sum )
