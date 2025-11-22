4e129fa8dfe4e3427dd9aefe7c622464a315a314
Anmol-prog-cyber <anmolbannu19@gmail.com>
2025-11-22 08:43:50 -0700

    Create Listopeartions.py

---
+++ Listopeartions.py
@@ -0,0 +1,12 @@
+Vacationlist=["Zurich","Boston","Hanover",]
+print(Vacationlist)
+for val in Vacationlist:
+    print(val)
+Vacationlist.append("Berlin")
+print(Vacationlist)
+Vacationlist.append("Colorado")
+print(Vacationlist)
+Vacationlist .remove("Colorado")
+print(Vacationlist)
+Vacationlist.clear()
+print(Vacationlist)
