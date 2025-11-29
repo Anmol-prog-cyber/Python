f4c00bcc038d3205bbcc08ea5796c9ffac59f554
Anmol-prog-cyber <anmolbannu19@gmail.com>
2025-11-29 08:57:32 -0700

    Create listoperatio2.py

---
+++ listoperatio2.py
@@ -0,0 +1,6 @@
+Booklist=["Atomic Habits","Outsiders","Wednesday Wars"]
+print(Booklist)
+Booklist.append("Barron's Algebra 1")
+print(Booklist)
+Booklist.remove("Barron's Algebra 1")
+print(Booklist)
