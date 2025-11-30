629c1c5431d3cb0c67260343a244cef8cdea34ee
Anmol-prog-cyber <anmolbannu19@gmail.com>
2025-11-30 08:14:42 -0700

    Create listofoperations3.py

---
+++ listofoperations.py
@@ -0,0 +1,5 @@
+Foodlist=["Pizza","Chocolate","biscuits,"]
+print(Foodlist[0])#Index is a source to describe strings with the help of numbers each string of characters has its own string associated i=wth zero .
+print(Foodlist[2])
+Foodlist[2]="Ice cream "
+print(Foodlist[2])
