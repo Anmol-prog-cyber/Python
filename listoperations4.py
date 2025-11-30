8b256a9e7313de02790285162bfe1087afab53bc
Anmol-prog-cyber <anmolbannu19@gmail.com>
2025-11-30 08:28:03 -0700

    Create listoperations4.py

---
+++ listoperations4.py
@@ -0,0 +1,7 @@
+Subjectlist=["MT","Science","Social studies","Computer Science"]
+print(Subjectlist[0])
+print(Subjectlist[1])
+print(Subjectlist[2])
+print(Subjectlist[3])
+Numberlist=[1]*7777
+print(Numberlist)
